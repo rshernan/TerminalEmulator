ea49400f56c585e33065061b4f52269cc793bc03
Fabrizio Ervini <faber.rvn@gmail.com>
2020-12-10 12:46:41 +0100

    Update rm.js

--- src/assets/js/modules/commands/rm.js
+++ src/assets/js/modules/commands/rm.js
@@ -3,7 +3,7 @@ import { dataStrc } from '../controllers/data.js';
 class Rm {
   executeComand(fileName) {
     if (fileName.includes('rm')) {
-      fileName.replace('rm', '');
+      fileName.replace('rm', '').trim();
     }
     dataStrc.deleteFolderOrDocument(fileName);
   }
