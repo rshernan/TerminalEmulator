62a8ebfae4d006016315bea9718f0b5fdf949138
fabrizio <faber.rvn@gmail.com>
2020-12-11 09:36:57 +0100

    Fix the css import

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,5 @@
 import { addSpan } from "./modules/renders/console_render.js";
-import { dataStrc } from "./modules/controllers/data.js";
-
+import "../css/style.css";
 
 let div = document.querySelector(".writed__input");
 
