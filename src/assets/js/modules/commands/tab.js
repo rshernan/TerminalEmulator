fc9b5e0b0b8fac8e773293bdeaaa7b25a12eb5ed
fabrizio <faber.rvn@gmail.com>
2020-12-13 11:49:31 +0100

    Update the autocomplete functions

--- src/assets/js/modules/commands/tab.js
+++ src/assets/js/modules/commands/tab.js
@@ -1,38 +1,56 @@
 import { dataStrc } from '../controllers/data';
 
-
 function autoComplete() {
-  // get the file from the actual location
-  let fileFromCurrentPath = dataStrc.getDataFromThisPath();
-  // trasform the object in array
-  let arrayFromFiles = Object.keys(fileFromCurrentPath.content);
-  // get the input to search with
-  let input = document.querySelector(".actual .writed__input").value;
+  let input = document.querySelector('.actual .writed__input').value;
+  let autoComplete;
   // check if is a path
   if (input.includes('/')) {
-    let currentPath = input.split('/')[0];
-    let searchIn = currentPath.split(" ")[1];
-    console.log(searchIn);
-  }
-  let filterFor = input.split(" ")[1];
-  // filter the arrau
-  let foundedFile = arrayFromFiles.filter( el => el.startsWith(filterFor));
-  // check the length
-  if (foundedFile.length > 1) {
-    // get the first result
-    let getFirstMatch = foundedFile[0];
-    // autocomplete
-    let completedName = input.replace(filterFor, getFirstMatch);
-    document.querySelector("input.writed__input").value = completedName;
-  } else {
-    // convert array to string
-    let stringFromFiles = foundedFile.join();
-    // autocomplete
-    let completedNames = input.replace(filterFor, stringFromFiles);
-    // render
-    document.querySelector("input.writed__input").value = completedNames;
-  }
+    // get the command and the folder
+    let currentInput = input.split('/')[0];
+    // get the folder to search in
+    let folder = currentInput.split(' ')[1];
+    // get the filter value
+    let filter = input.split('/')[1];
+    // get file in folder
+    let searchInFolder = dataStrc.openFolderInPath(folder);
 
+    if (!searchInFolder) {
+      let fileInFolder = dataStrc.getDataFromThisPath();
+      let arrayFromFile = Object.keys(fileInFolder.content);
+      let filteredArray = arrayFromFile.filter(el => el.startsWith(filter));
+
+      if (filteredArray.length > 1) {
+        let getFirstMatch = filteredArray[0];
+        autoComplete = filter = getFirstMatch;
+        document.querySelector("input.writed__input").value = `${currentInput}/${autoComplete}`;
+      } else if (filteredArray.length === 1) {
+        let arrayToString = filteredArray.join();
+        autoComplete = filter = arrayToString;
+        document.querySelector("input.writed__input").value = `${currentInput}/${autoComplete}`;
+      } else {
+        return input;
+      }
+    }
+
+  } else {
+    let fileInCurrentPath = dataStrc.getDataFromThisPath();
+    let arrayFromFiles = Object.keys(fileInCurrentPath.content);
+    let currentInput = input.split(' ')[0];
+    let filter = input.split(' ')[1];
+    let foundedFiles = arrayFromFiles.filter(el => el.startsWith(filter));
+
+    if (foundedFiles.length > 1) {
+      let getFirstMatch = foundedFiles[0];
+      autoComplete = filter = getFirstMatch;
+      document.querySelector("input.writed__input").value = `${currentInput} ${autoComplete}`;
+    } else if (foundedFiles.length === 1) {
+      let arrayToString = foundedFiles.join();
+      autoComplete = filter = arrayToString;
+      document.querySelector("input.writed__input").value = `${currentInput} ${autoComplete}`;
+    } else {
+      return input;
+    }
+  }
 }
 
 
