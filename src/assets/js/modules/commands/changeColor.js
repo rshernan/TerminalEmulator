6945b71600b1ba59940eef91258bed8fb20fa1e9
rshernan <rsolerhernan@gmail.com>
2020-12-14 13:23:15 +0100

    completed change themes

--- src/assets/js/modules/commands/changeColor.js
+++ src/assets/js/modules/commands/changeColor.js
@@ -8,17 +8,46 @@ class ChangeColor {
         let color = command.split(" ")[2];
         console.log(variable);
         console.log(color);
-        switch (variable) {
-            case "text":
-                root.style.setProperty("--color-text", color);
-                break;
-            case "path":
-                root.style.setProperty("--color-path", color);
-                break;
-            case "background":
-                root.style.setProperty("--color-background", color);
-                break;
+        if (this.validColour(color)) {
+            switch (variable) {
+                case "text":
+                    root.style.setProperty("--color-text", color);
+                    break;
+                case "path":
+                    root.style.setProperty("--color-path", color);
+                    break;
+                case "background":
+                    root.style.setProperty("--color-background", color);
+                    break;
+                default:
+                    console.log("invalid parameter");
+            }
+        } else {
+            console.log("invalid color");
         }
+    }
+
+    validColour(stringToTest) {
+        //Alter the following conditions according to your need.
+        if (stringToTest === "") {
+            return false;
+        }
+        if (stringToTest === "inherit") {
+            return false;
+        }
+        if (stringToTest === "transparent") {
+            return false;
+        }
+
+        var image = document.createElement("img");
+        image.style.color = "rgb(0, 0, 0)";
+        image.style.color = stringToTest;
+        if (image.style.color !== "rgb(0, 0, 0)") {
+            return true;
+        }
+        image.style.color = "rgb(255, 255, 255)";
+        image.style.color = stringToTest;
+        return image.style.color !== "rgb(255, 255, 255)";
     }
 }
 
--- src/assets/js/modules/controllers/commandController.js
+++ src/assets/js/modules/controllers/commandController.js
@@ -11,6 +11,7 @@ import { Mv } from "../commands/mv.js";
 import { Touch } from "../commands/touch.js";
 import { Man } from "../commands/man.js";
 import { ChangeColor } from "../commands/changeColor.js";
+import { ChangeTheme } from "../commands/changeTheme.js";
 
 class CommandController {
     constructor() {}
@@ -82,9 +83,12 @@ class CommandController {
                 output = touch.executeComand(writedLine);
                 break;
             case "color":
-                console.log(writedLine);
                 let changeColor = new ChangeColor();
                 changeColor.execute(writedLine);
+                break;
+            case "theme":
+                let changeTheme = new ChangeTheme();
+                changeTheme.execute(writedLine);
                 break;
             default:
                 console.log("command not found");
